b827e543ca2a3b65c34d1f8b6a67b95f573c5c23
Rodrick Heffley <codemonkey25@outlook.com>
2025-11-20 21:55:57 -0500

    deleted files

--- JS/JS lib/Global/Modules/footer-module.js
+++
@@ -1,7 +0,0 @@
-export function loaderfooter() {
-    return fetch('/Assets/Modules/footer.html')
-                .then(res => res.text())
-                .then(data => {
-                    document.getElementById('footer-module').innerHTML = data;
-                })
-}
--- JS/JS lib/Global/Modules/header-module.js
+++
@@ -1,7 +0,0 @@
-export function loadHeader() {
-    return fetch('/Assets/Modules/header.html')
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById('header-module').innerHTML = data;
-        });
-}
