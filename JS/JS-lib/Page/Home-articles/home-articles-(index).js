c7f8afb800afe214ea27cba80dc9b2bdce88c0b9
Rodrick Heffley <codemonkey25@outlook.com>
2025-11-28 10:26:14 -0500

    created fiile and code for home-articles-(index) JS

---
+++ JS/JS-lib/Page/Home-articles/home-articles-(index).js
@@ -0,0 +1,9 @@
+import { initArticles } from "/JS/JS-lib/Page/Home-articles/home-articles-card-logic.js";
+
+    initArticles(
+        "/Database/placeholder-data3.csv",
+        "home-articles",
+        "row-template",
+        "load-more"
+    );
+
