7d1bc121b37dd3fa9978555d0bfc54e3c8f4116f
Rodrick Heffley <codemonkey25@outlook.com>
2025-11-29 16:45:40 -0500

    updated the code for home JS

--- JS/JS-lib/Page/Home/home.js
+++ JS/JS-lib/Page/Home/home.js
@@ -1,7 +1,7 @@
 import { loadHomeCards } from "/JS/JS-lib/Page/Home/home-articles.js";
 
 loadHomeCards(
-    "/Database/placeholder-data2.csv",        // CSV file
+    "/Database/placeholder-data3.csv",        // CSV file
     ".home-cards-wrapper",        // container
     4                              // limit
 );
